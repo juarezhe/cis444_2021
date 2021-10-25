f6118c95a66d7f83a8c9969969402a6bc255ccc2
Hannah Juarez <23268165+juarezhe@users.noreply.github.com>
2021-10-25 09:06:46 -0700

    Visual clarity for login and signup tabs.

--- hello_flask/assignemt3.py
+++ hello_flask/assignemt3.py
@@ -1,18 +1,61 @@
 from flask import Flask, render_template, request
-from flask_json import FlaskJSON
+from flask_json import FlaskJSON, json_response
 from bookstore_con import get_db
-
+import jwt
 import bcrypt
 
 app = Flask(__name__)
 FlaskJSON(app)
 
 global_db_con = get_db()
+#JWT_SECRET = "Test"
+#JWT_TOKEN = None
+
+def getBooks():
+    cur = global_db_con.cursor()
+    cur.execute("select * from books;")
+    message = '{'
+    while 1:
+        row = cur.fetchone()
+        if row is None:
+            print("Error: no books exist yet")
+            break
+        else:
+            message += row
+    message += '}'
+    print(message)
+    return
+
+
+def getToken():
+    token = request.args.get('jwt')
+    if token is None:
+        return False
+    print("Token found: " + token)
+    return token
+
+
+def tokenIsValid():
+    token = getToken()
+    if token == False:
+        print("Token does not exist.")
+        return False
+    # TODO: Some kind of test for the token
+    # print(json_response(output=jwt.decode(
+    #    token, JWT_SECRET, algorithms=["HS256"])))
+    return True
+
+
+def validateUser():
+    if tokenIsValid():
+        print("User is authenticated.")
+        return render_template('index.html')  # render for authenticated users?
+    return render_template('index.html')  # render the base template?
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return validateUser()
 
 
 @app.route('/<path:text>', methods=['GET', 'POST'])
@@ -21,17 +64,23 @@ def all_routes(text):
         cur = global_db_con.cursor()
         form_salted = bcrypt.hashpw(
             bytes(request.form['login_pass'], 'utf-8'), bcrypt.gensalt(8))
-        cur.execute("select password from users where username = '" +
+        cur.execute("select * from users where username = '" +
                     request.form['login_user'] + "';")
         fetch = cur.fetchone()
         if fetch is None:
-            print('Error - username "' +
+            print('Error: username "' +
                   request.form['login_user'] + '" does not exist.')
         else:
-            if bcrypt.checkpw(form_salted, bytes(fetch[0], 'utf-8')) == 'false':
-                print('Error - passwords do not match')
+            if bcrypt.checkpw(form_salted, bytes(fetch[2], 'utf-8')) == 'false':
+                print('Error: passwords do not match')
             else:
                 print('Login by user ' + request.form['login_user'])
+                #JWT_TOKEN = {"userId": fetch[0]}
+    
+    #if text == 'fetchToken':
+    #    if JWT_TOKEN is None:
+    #        return index()
+    #    return json_response(jwt=jwt.encode(JWT_TOKEN, JWT_SECRET, algorithm="HS256"))
 
     if text == 'signup':
         cur = global_db_con.cursor()
@@ -44,13 +93,13 @@ def all_routes(text):
                         request.form['signup_user'] + "','" + salted.decode("utf-8") + "');commit;")
             print('Created user ' + request.form['signup_user'])
         else:
-            print('Error - username "' +
+            print('Error: username "' +
                   request.form['signup_user'] + '" already in use.')
 
-    return index()
+    if text == 'getBooks':
+        getBooks()
 
-# For deleting user account
-# delete from users where user_id = 0;
+    return index()
 
 
 app.run(host='0.0.0.0', port=80)

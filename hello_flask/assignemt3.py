148eadf1f5c859df6ec5ea81ff5054c712e61e46
Hannah Juarez <23268165+juarezhe@users.noreply.github.com>
2021-10-25 13:30:31 -0700

    Refactor endpoints. Pass token to front-end.

--- hello_flask/assignemt3.py
+++ hello_flask/assignemt3.py
@@ -8,8 +8,9 @@ app = Flask(__name__)
 FlaskJSON(app)
 
 global_db_con = get_db()
-#JWT_SECRET = "Test"
-#JWT_TOKEN = None
+JWT_SECRET = "Test"
+JWT_TOKEN = None
+
 
 def getBooks():
     cur = global_db_con.cursor()
@@ -29,9 +30,9 @@ def getBooks():
 
 def getToken():
     token = request.args.get('jwt')
+    print(token)
     if token is None:
         return False
-    print("Token found: " + token)
     return token
 
 
@@ -41,64 +42,60 @@ def tokenIsValid():
         print("Token does not exist.")
         return False
     # TODO: Some kind of test for the token
-    # print(json_response(output=jwt.decode(
-    #    token, JWT_SECRET, algorithms=["HS256"])))
+    print(json_response(output=jwt.decode(
+        token, JWT_SECRET, algorithms=["HS256"])))
     return True
 
 
-def validateUser():
-    if tokenIsValid():
-        print("User is authenticated.")
-        return render_template('index.html')  # render for authenticated users?
-    return render_template('index.html')  # render the base template?
-
-
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
-    return validateUser()
+    return render_template('index.html')
 
 
-@app.route('/<path:text>', methods=['GET', 'POST'])
-def all_routes(text):
-    if text == 'login':
-        cur = global_db_con.cursor()
-        form_salted = bcrypt.hashpw(
-            bytes(request.form['login_pass'], 'utf-8'), bcrypt.gensalt(8))
-        cur.execute("select * from users where username = '" +
-                    request.form['login_user'] + "';")
-        fetch = cur.fetchone()
-        if fetch is None:
-            print('Error: username "' +
-                  request.form['login_user'] + '" does not exist.')
+@app.route('/fetchToken', methods=['GET'])
+def fetchToken():
+    global JWT_TOKEN
+    if JWT_TOKEN is None:
+        return index()
+    return json_response(jwt=JWT_TOKEN)
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    cur = global_db_con.cursor()
+    form_salted = bcrypt.hashpw(
+        bytes(request.form['login_pass'], 'utf-8'), bcrypt.gensalt(8))
+    cur.execute("select * from users where username = '" +
+                request.form['login_user'] + "';")
+    row = cur.fetchone()
+    if row is None:
+        print('Error: username "' +
+              request.form['login_user'] + '" does not exist.')
+    else:
+        if bcrypt.checkpw(form_salted, bytes(row[2], 'utf-8')) == 'false':
+            print('Error: passwords do not match')
         else:
-            if bcrypt.checkpw(form_salted, bytes(fetch[2], 'utf-8')) == 'false':
-                print('Error: passwords do not match')
-            else:
-                print('Login by user ' + request.form['login_user'])
-                #JWT_TOKEN = {"userId": fetch[0]}
-    
-    #if text == 'fetchToken':
-    #    if JWT_TOKEN is None:
-    #        return index()
-    #    return json_response(jwt=jwt.encode(JWT_TOKEN, JWT_SECRET, algorithm="HS256"))
+            print('Login by user ' + request.form['login_user'])
+            global JWT_TOKEN, JWT_SECRET
+            JWT_TOKEN = jwt.encode(
+                {"userId": row[0]}, JWT_SECRET, algorithm="HS256")
+    return index()
 
-    if text == 'signup':
-        cur = global_db_con.cursor()
-        cur.execute("select username from users where username = '" +
-                    request.form['signup_user'] + "';")
-        if cur.fetchone() is None:
-            salted = bcrypt.hashpw(
-                bytes(request.form['signup_pass'], 'utf-8'), bcrypt.gensalt(8))
-            cur.execute("insert into users values (nextval('users_user_id_seq'::regclass),'" +
-                        request.form['signup_user'] + "','" + salted.decode("utf-8") + "');commit;")
-            print('Created user ' + request.form['signup_user'])
-        else:
-            print('Error: username "' +
-                  request.form['signup_user'] + '" already in use.')
 
-    if text == 'getBooks':
-        getBooks()
-
+@app.route('/signup', methods=['POST'])
+def signup():
+    cur = global_db_con.cursor()
+    cur.execute("select username from users where username = '" +
+                request.form['signup_user'] + "';")
+    if cur.fetchone() is None:
+        salted = bcrypt.hashpw(
+            bytes(request.form['signup_pass'], 'utf-8'), bcrypt.gensalt(8))
+        cur.execute("insert into users values (nextval('users_user_id_seq'::regclass),'" +
+                    request.form['signup_user'] + "','" + salted.decode("utf-8") + "');commit;")
+        print('Created user ' + request.form['signup_user'])
+    else:
+        print('Error: username "' +
+              request.form['signup_user'] + '" already in use.')
     return index()
 
 

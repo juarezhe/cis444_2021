10e556fca46768ede739d23790c4effee7298fd6
Hannah Juarez <23268165+juarezhe@users.noreply.github.com>
2021-10-26 12:24:13 -0700

    Document APIs. Add secret file functionality for web token.

--- hello_flask/assignemt3.py
+++ hello_flask/assignemt3.py
@@ -8,24 +8,10 @@ app = Flask(__name__)
 FlaskJSON(app)
 
 global_db_con = get_db()
-JWT_SECRET = "Test"
 JWT_TOKEN = None
-
-
-def getBooks():
-    cur = global_db_con.cursor()
-    cur.execute("select * from books;")
-    message = '{'
-    while 1:
-        row = cur.fetchone()
-        if row is None:
-            print("Error: no books exist yet")
-            break
-        else:
-            message += row
-    message += '}'
-    print(message)
-    return
+JWT_SECRET = None
+with open("secret.txt", "r") as f:
+    JWT_SECRET = f.read()
 
 
 @app.route('/', methods=['GET'])
@@ -33,28 +19,87 @@ def index():
     return render_template('index.html')
 
 
+#---------------------------Start Item APIs-------------------------------#
+# Accepts aone or more books for adding to the database.
+#
+# Returns the successfulness of the database add.
+@app.route('/addBooks', methods=['POST'])
+def addBooks():
+    cur = global_db_con.cursor()
+    cur.execute("select * from books;")
+    message = '{'
+    while 1:
+        row = cur.fetchone()
+        if row is None:
+            break
+        else:
+            message += row
+    message += '}'
+    print(message)
+    return json_response(data=message)
+
+
+# Returns the entire list of all books in the database.
+@app.route('/fetchBookList', methods=['GET'])
+def fetchBookList():
+    cur = global_db_con.cursor()
+    cur.execute("select * from books;")
+    message = '{'
+    while 1:
+        row = cur.fetchone()
+        if row is None:
+            break
+        else:
+            message += row
+    message += '}'
+    print(message)
+    return json_response(data=message)
+#----------------------------End Item APIs--------------------------------#
+
+
+#--------------------------Start Token APIs-------------------------------#
+# Returns a web token if a valid one exists. Otherwise, returns an error
+# message.
 @app.route('/fetchToken', methods=['GET'])
 def fetchToken():
     global JWT_TOKEN
     if JWT_TOKEN is None:
-        print("Token does not exist. Loading index page.")
-        return index()
-    print("Token exists. Sending token.")
+        print("Token does not exist. Sending error message.")
+        return json_response(data={"Error": "No token stored in server."})
     return json_response(jwt=JWT_TOKEN)
 
 
+# Accepts a web token.
+#
+# Returns the successfulness of the token comparison. On success, returns
+# the same web token? On failure, returns an error message.
 @app.route('/validateToken', methods=['POST'])
 def validateToken():
     global JWT_TOKEN, JWT_SECRET
+
+    # If the token is still None, it hasn't been set. Don't attempt to
+    # validate.
+    if JWT_TOKEN is None:
+        print("Token does not exist. Sending error message.")
+        return json_response(data={"Error": "No token stored in server."})
+
     local_decode = jwt.decode(JWT_TOKEN, JWT_SECRET, algorithms=["HS256"])
-    post_decode = jwt.decode(request.form['jwt'], JWT_SECRET, algorithms=["HS256"])
+    post_decode = jwt.decode(
+        request.form['jwt'], JWT_SECRET, algorithms=["HS256"])
     if local_decode == post_decode:
-        print("Tokens match. Sending response.")
+        print("Tokens match. Sending success message.")
         return json_response(data=request.form)
-    print("Tokens do not match. Loading index page.")
-    return index()
+
+    print("Tokens do not match. Sending error message.")
+    return json_response(data={"Error": "Tokens do not match."})
+#---------------------------End Token APIs--------------------------------#
 
 
+#-------------------------Start Account APIs------------------------------#
+# Accepts a username and password for authentication against database.
+#
+# Returns successfulness of authentication. On success, returns a web
+# token? On failure, returns an error message.
 @app.route('/login', methods=['POST'])
 def login():
     cur = global_db_con.cursor()
@@ -64,17 +109,22 @@ def login():
                 request.form['login_user'] + "';")
     row = cur.fetchone()
     if row is None:
-        print('Error: username "' +
-              request.form['login_user'] + '" does not exist.')
+        print('Username "' + request.form['login_user'] +
+              '" does not exist. Sending error message.')
+        return json_response(data={"Error": "Username '" + request.form['login_user'] + "' does not exist."})
     else:
         if bcrypt.checkpw(form_salted, bytes(row[2], 'utf-8')) == 'false':
-            print('Error: passwords do not match')
+            print('Passwords do not match. Sending error message.')
+            return json_response(data={"Error": "Passwords do not match."})
         else:
             print('Login by user ' + request.form['login_user'])
             global JWT_TOKEN, JWT_SECRET
             JWT_TOKEN = jwt.encode(
                 {"userId": row[0]}, JWT_SECRET, algorithm="HS256")
-    return index()
+            # TODO: This should be a valid message.
+            # Potentially return the token itself in this message.
+            # return json_response(data={"Error": "Unknown error."})
+    return json_response(data={"Error": "Unknown error."}) # TODO: Remove this when above is addressed.
 
 
 @app.route('/signup', methods=['POST'])
@@ -92,6 +142,7 @@ def signup():
         print('Error: username "' +
               request.form['signup_user'] + '" already in use.')
     return index()
+#---------------------------End Account APIs------------------------------#
 
 
 app.run(host='0.0.0.0', port=80)

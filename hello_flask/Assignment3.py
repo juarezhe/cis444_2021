ed4e47585add5d1aa363c366de30fd5feae8a1d2
Hannah Juarez <23268165+juarezhe@users.noreply.github.com>
2021-11-03 21:18:15 -0700

    Update connection to read from file. Change backend variable to in-line.

--- hello_flask/Assignment3.py
+++ hello_flask/Assignment3.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
 from flask_json import FlaskJSON, json_response
-from BookstoreCon import GetDb
+from BookstoreConn import GetDb
 import bcrypt
 import json
 import jwt
@@ -22,91 +22,109 @@ except:
 
 
 def ValidateToken(token):
+    print("ValidateToken() entry")
+
     # If the token is still None, it hasn"t been set. Don"t attempt to
     # validate.
     if JWT_TOKEN is None:
         return False
     else:
+        fromClient = jwt.decode(token, JWT_SECRET, algorithms=["HS256"])
         fromServer = jwt.decode(JWT_TOKEN, JWT_SECRET, algorithms=["HS256"])
-        fromRequest = jwt.decode(token, JWT_SECRET, algorithms=["HS256"])
-        return fromServer == fromRequest
+        return fromClient == fromServer
 
 
 # Default endpoint.
 @app.route("/", methods=["GET"])
+@app.route("/index", methods=["GET"])
+@app.route("/index.html", methods=["GET"])
 def Index():
+    print("Index() entry")
+
     return render_template("index.html")
 
 
 #--------------------------Start Token APIs-------------------------------#
 @ app.route("/logout", methods=["GET"])
 def Logout():
+    print("Logout() entry")
+
     if ValidateToken(request.args.get("jwt")):
+        # TODO: Remove token from list of valid, active tokens?
         global JWT_TOKEN
         JWT_TOKEN = None
-
-        print("User logged out. Sending success message.")
+        print("User logged out. Sending response.")
         return json_response(data={"message": "User logged out."})
     else:
-        print("Invalid token. Sending error message.")
-        return json_response(data={"message": "User is not logged in."}, status=404)
+        print("User not logged in. Sending response.")
+        return json_response(data={"message": "Already logged out."})
 #---------------------------End Token APIs--------------------------------#
 
 
 #---------------------------Start Item APIs-------------------------------#
-# Accepts a user ID and a book ID and creates a record of the purchase.
+# Accepts a JWT and a book ID. Upon successful validation of the JWT,
+# creates a record of the purchase.
 #
 # Returns the successfulness of the database add.
 @app.route("/buyBook", methods=["GET"])
 def BuyBook():
-    token = request.args.get("jwt")
-    if ValidateToken(token):
-        decodedToken = jwt.decode(token, JWT_SECRET, algorithms=["HS256"])
-        cursor = GLOBAL_DB_CON.cursor()
+    print("BuyBook() entry")
 
-        try:
-            cursor.execute("insert into purchases values (nextval('purchases_transaction_seq'::regclass),'" + str(
-                decodedToken["user_id"]) + "','" + str(request.args.get("book_id")) + "','" + str(datetime.datetime.now()) + "'); commit;")
-            print("Purchase success. Sending message.")
-            return json_response(data={"message": "Book bought successfully."})
-        except:
-            return json_response(data={"message": "Error occured while writing to database."}, status=500)
-    else:
-        print("Invalid token. Sending error message.")
+    token = request.args.get("jwt")
+    if not token:
+        print("Missing token. Sending response.")
+        return json_response(data={"message": "Missing token."}, status=404)
+
+    if not ValidateToken(token):
+        print("Invalid token. Sending response.")
         return json_response(data={"message": "User is not logged in."}, status=404)
+
+    decodedToken = jwt.decode(token, JWT_SECRET, algorithms=["HS256"])
+    cursor = GLOBAL_DB_CON.cursor()
+    try:
+        cursor.execute(f"insert into purchases values (nextval('purchases_transaction_seq'::regclass),'" +
+                       f"{str(decodedToken['user_id'])}','{str(request.args.get('book_id'))}','" +
+                       f"{str(datetime.datetime.now())}'); commit;")
+        print("Purchase success. Sending response.")
+        return json_response(data={"message": "Book bought successfully."})
+    except:
+        print("Database write error. Sending response.")
+        return json_response(data={"message": "Error occured while writing to database."}, status=500)
 
 
 # Returns a list of all books in the database.
 @ app.route("/getBookList", methods=["GET"])
 def GetBookList():
-    if ValidateToken(request.args.get("jwt")):
-        cursor = GLOBAL_DB_CON.cursor()
+    print("GetBookList() entry")
 
-        try:
-            cursor.execute("select lpad(book_id::varchar(" + str(BOOK_ID_LENGTH) +
-                           "), " + str(BOOK_ID_LENGTH) + ", '0'), title, price from books;")
-        except:
-            return json_response(data={"message": "Error occured while reading from database."}, status=500)
-
-        count = 0
-        message = '{"books":['
-        while 1:
-            row = cursor.fetchone()
-            if row is None:
-                break
-            else:
-                if count > 0:
-                    message += ","
-                message += '{"book_id":"' + row[0] + '","title":"' + row[1] + \
-                    '","price":' + str(row[2]) + "}"
-                count += 1
-        message += "]}"
-
-        print("Valid token. Sending list of books.")
-        return json_response(data=json.loads(message))
-    else:
-        print("Invalid token. Sending error message.")
+    if not ValidateToken(request.args.get("jwt")):
+        print("Invalid token. Sending response.")
         return json_response(data={"message": "User is not logged in."}, status=404)
+
+    cursor = GLOBAL_DB_CON.cursor()
+    try:
+        cursor.execute(f"select lpad(book_id::varchar({str(BOOK_ID_LENGTH)}), " +
+                       f"{str(BOOK_ID_LENGTH)}, '0'), title, price from books;")
+    except:
+        print("Database read error. Sending response.")
+        return json_response(data={"message": "Error occured while reading from database."}, status=500)
+
+    count = 0
+    message = '{"books":['
+    while 1:
+        row = cursor.fetchone()
+        if row is None:
+            break
+        else:
+            if count > 0:
+                message += ","
+            message += '{"book_id":"' + row[0] + '","title":"' + \
+                row[1] + '","price":' + str(row[2]) + "}"
+            count += 1
+    message += "]}"
+
+    print("Valid token. Sending list of books.")
+    return json_response(data=json.loads(message))
 #----------------------------End Item APIs--------------------------------#
 
 
@@ -116,31 +134,39 @@ def GetBookList():
 # Returns message indicating successfulness of authentication process.
 @app.route("/login", methods=["POST"])
 def Login():
+    print("Login() entry")
+
+    username = request.form["username"]
+    if not username:
+        return json_response(data={"message": "Missing username."}, status=404)
+
+    password = request.form["password"]
+    if not password:
+        return json_response(data={"message": "Missing password."}, status=404)
+
     cursor = GLOBAL_DB_CON.cursor()
     try:
         # Pad with leading zeros up to 8 characters.
-        cursor.execute("select lpad(user_id::varchar(" + str(USER_ID_LENGTH) + "), " + str(USER_ID_LENGTH) +
-                       ", '0'), username, password from users where username = '" + request.form["username"] + "';")
+        cursor.execute(f"select lpad(user_id::varchar({str(USER_ID_LENGTH)}), {str(USER_ID_LENGTH)}" +
+                       f", '0'), username, password from users where username = '{username}';")
     except:
+        print("Database read error. Sending response.")
         return json_response(data={"message": "Error occured while reading from database."}, status=500)
-    row = cursor.fetchone()
 
-    if row is None:
-        print("Username '" + request.form["username"] +
-              "' does not exist. Sending error message.")
-        return json_response(data={"message": "Username '" +
-                                   request.form["username"] + "' does not exist."}, status=404)
-    else:
-        if bcrypt.checkpw(bytes(request.form["password"], "utf-8"), bytes(row[2], "utf-8")) == True:
-            print("Login by user '" + request.form["username"] + "'.")
+    row = cursor.fetchone()
+    if row is not None:
+        if bcrypt.checkpw(bytes(password, "utf-8"), bytes(row[2], "utf-8")) == True:
             global JWT_TOKEN
-            JWT_TOKEN = jwt.encode(
-                {"user_id": row[0]}, JWT_SECRET, algorithm="HS256")
+            JWT_TOKEN = jwt.encode({"user_id": row[0], "username": row[1], "timestamp": str(datetime.datetime.now())},
+                                   JWT_SECRET, algorithm="HS256")
+            print(f"Login by user '{username}'. Sending Token.")
             return json_response(data={"jwt": JWT_TOKEN})
         else:
-            print("Incorrect password. Sending error message.")
-            return json_response(
-                data={"message": "Incorrect password."}, status=404)
+            print("Incorrect password. Sending response.")
+            return json_response(data={"message": "Incorrect password."}, status=404)
+    else:
+        print(f"Username '{username}' does not exist. Sending response.")
+        return json_response(data={"message": f"Username '{username}' does not exist."}, status=404)
 
 
 # Accepts username and password for adding to the database.
@@ -148,28 +174,35 @@ def Login():
 # Returns message indicating successfulness of user creation.
 @app.route("/signup", methods=["POST"])
 def Signup():
+    print("Signup() entry")
+
+    username = request.form["username"]
+    if not username:
+        return json_response(data={"message": "Missing username."}, status=404)
+
+    password = request.form["password"]
+    if not password:
+        return json_response(data={"message": "Missing password."}, status=404)
+
     cursor = GLOBAL_DB_CON.cursor()
     try:
-        cursor.execute("select * from users where username = '" +
-                       request.form["username"] + "';")
+        cursor.execute(f"select * from users where username = '{username}';")
     except:
         return json_response(data={"message": "Error occured while reading from database."}, status=500)
 
     if cursor.fetchone() is None:
-        saltedPass = bcrypt.hashpw(
-            bytes(request.form["password"], "utf-8"), bcrypt.gensalt(8))
+        saltedPass = bcrypt.hashpw(bytes(password, "utf-8"), bcrypt.gensalt(8))
         try:
-            cursor.execute("insert into users values (nextval('users_user_id_seq'::regclass),'" +
-                           request.form["username"] + "','" + saltedPass.decode("utf-8") + "'); commit;")
-            print("Created user " + request.form["username"] + ".")
-            return json_response(data={"message": "User created successfully."})
+            cursor.execute(f"insert into users values (nextval('users_user_id_seq'::regclass),'" +
+                           f"{username}','{saltedPass.decode('utf-8')}'); commit;")
+            print(f"Created user '{username}'.")
+            return json_response(data={"message": f"User '{username}' created successfully."})
         except:
+            print("Database error occurre. Sending response.")
             return json_response(data={"message": "Error occured while writing to database."}, status=500)
     else:
-        print("Username '" + request.form["username"] +
-              "' already in use. Sending error message.")
-        return json_response(data={"message": "Username '" +
-                                   request.form["username"] + "' already in use."}, status=404)
+        print(f"Username '{username}' already in use. Sending response.")
+        return json_response(data={"message": f"Username '{username}' already in use."}, status=404)
 #---------------------------End Account APIs------------------------------#
 
 
--- hello_flask/BookstoreCon.py
+++
@@ -1,5 +0,0 @@
-import psycopg2
-
-
-def GetDb():
-    return psycopg2.connect(host="localhost", dbname="bookstore", user="user_auth", password="k*g7hu+M*?WSxk7F")

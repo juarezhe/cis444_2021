13bca732e60351db0cb343e2fdc279ddde1f7683
Hannah J <hb959@outlook.com>
2025-01-24 16:21:28 -0800

    Replace deprecated function

--- assignment4/tools/token_tools.py
+++ assignment4/tools/token_tools.py
@@ -1,12 +1,9 @@
 import jwt
 import datetime
 from flask import  g
-from tools.logging import logger
-
 
 def create_token(token_data):
-    token_data['exp'] = datetime.datetime.utcnow() + datetime.timedelta(days=0, minutes=30)
-    token_data['iat'] = datetime.datetime.utcnow()
+    token_data['exp'] = datetime.datetime.now(datetime.timezone.utc) + datetime.timedelta(days=0, minutes=30)
+    token_data['iat'] = datetime.datetime.now(datetime.timezone.utc)
 
     return jwt.encode( token_data , g.secrets['JWT'],  algorithm="HS256")
-
